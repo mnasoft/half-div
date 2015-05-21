5d40da2ea4b75b0e73f868576a262ffd0901f94b
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-21 07:38:03 +0300

    123

--- package.lisp
+++ package.lisp
@@ -2,6 +2,6 @@
 
 (defpackage #:half-div
   (:use #:cl)
-  (:export h-div)
-  (:export h-div-lst)) 
-
+  (:export h-div) ;;Функция решения уравнения func(x)=0 методом половинного деления.
+  (:export h-div-lst) ;;Функция решения уравнения func(X[0],X[1],...,X[n],...,X[m])=0 методом половинного деления.
+  )
