1bb26ea20cf661ad39feeb3775d77071ddbaae31
Mykola Matvyeyev <mnasoft@gmail.com>
2023-03-12 20:06:04 +0200

    Sun 12 Mar 2023 06:06:04 PM UTC

--- package.lisp
+++ package.lisp
@@ -6,7 +6,4 @@
   (:export h-div-lst) ;;Функция решения уравнения func(X[0],X[1],...,X[n],...,X[m])=0 методом половинного деления.
   )
 
-
-;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
-
-;;;; (declaim (optimize (compilation-speed 0) (debug 3) (safety 0) (space 0) (speed 0)))
+(in-package :half-div)
